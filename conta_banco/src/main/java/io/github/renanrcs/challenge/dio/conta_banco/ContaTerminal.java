5399c289058697edda5b515cc64e1474e9edb68f
renanrcs <renan.cavalcante@yahoo.com>
2024-05-04 11:15:47 -0300

    projeto conta banco

---
+++ conta_banco/src/main/java/io/github/renanrcs/challenge/dio/conta_banco/ContaTerminal.java
@@ -0,0 +1,13 @@
+package io.github.renanrcs.challenge.dio.conta_banco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaTerminal {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContaTerminal.class, args);
+	}
+
+}
---
+++ conta_banco/src/test/java/io/github/renanrcs/challenge/dio/conta_banco/ContaBancoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.renanrcs.challenge.dio.conta_banco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContaBancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
