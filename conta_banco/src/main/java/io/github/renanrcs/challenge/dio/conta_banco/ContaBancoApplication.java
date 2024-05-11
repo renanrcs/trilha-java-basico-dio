e05706fb0988b5ca536bcc2a9b12f904f2b69ba7
renanrcs <renan.cavalcante@yahoo.com>
2024-05-11 16:07:34 -0300

    Aplicando encapsulamento

--- conta_banco/src/main/java/io/github/renanrcs/challenge/dio/conta_banco/ContaBancoApplication.java
+++ conta_banco/src/main/java/io/github/renanrcs/challenge/dio/conta_banco/ContaBancoApplication.java
@@ -12,27 +12,12 @@ import io.github.renanrcs.challenge.dio.conta_banco.model.ContaBanco;
 public class ContaBancoApplication {
 
 	public static void main(String[] args) {
-		
 		SpringApplication.run(ContaBancoApplication.class, args);
 
 		ContaBanco contaBanco = new ContaBanco();
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.println("Por favor, digite o nome do cliente !");
-		String nomeDoCliente = scanner.nextLine();
-		contaBanco.setNameClient(nomeDoCliente); 
 
-		System.out.println("Por favor, digite o número da Conta !");
-		Long numeroDaConta = scanner.nextLong();
-		contaBanco.setNumber(numeroDaConta);
-		
-		System.out.println("Por favor, Informe o valor do deposito !");
-		BigDecimal valorDeposito = scanner.nextBigDecimal();
-		contaBanco.setAmount(valorDeposito); 
+		contaBanco.criarConta();
 
-		System.out.println(contaBanco);
-
-		scanner.close();
 	}
 
 }
--- conta_banco/src/main/java/io/github/renanrcs/challenge/dio/conta_banco/model/ContaBanco.java
+++ conta_banco/src/main/java/io/github/renanrcs/challenge/dio/conta_banco/model/ContaBanco.java
@@ -1,6 +1,7 @@
 package io.github.renanrcs.challenge.dio.conta_banco.model;
 
 import java.math.BigDecimal;
+import java.util.Scanner;
 
 import org.springframework.stereotype.Component;
 
@@ -14,16 +15,44 @@ import lombok.Setter;
 @NoArgsConstructor
 @Component
 public class ContaBanco {
-
+    private static Scanner scanner = new Scanner(System.in);
     private static final String AGENCY = "067-8";
 
     private Long number;
     private String nameClient;
     private BigDecimal amount;
 
+    public void criarConta() {
+        cadastrarCliente();
+        cadastrarNumeroConta();
+        depositar();
+        System.out.println(toString());
+    }
+
+    private void cadastrarCliente() {
+        System.out.println("Por favor, digite o nome do cliente !");
+        String nomeDoCliente = scanner.nextLine();
+        setNameClient(nomeDoCliente); 
+    }
+
+    private void cadastrarNumeroConta() {
+        System.out.println("Por favor, digite o número da Conta !");
+        Long numeroDaConta = scanner.nextLong();
+        setNumber(numeroDaConta);
+    }
+
+    private void depositar() {
+        System.out.println("Por favor, Informe o valor do deposito !");
+        BigDecimal valorDeposito = scanner.nextBigDecimal();
+        scanner.close();
+        setAmount(valorDeposito); 
+    }
+
+
     @Override
     public String toString() {
         return "Olá " + getNameClient() + ", obrigado por criar uma conta em nosso banco, sua agência é " + ContaBanco.AGENCY + ", conta " + getNumber() + " e seu saldo R$" + getAmount() + " já está disponível para saque";
     }
+
 
 }
